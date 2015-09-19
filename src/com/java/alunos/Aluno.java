73b9bf56163485a178187dc8084ec021fcfdffb2
njr1466 <njr1466@gmail.com>
2015-09-19 14:57:44 -0300

    first commit

---
+++ src/com/java/alunos/Aluno.java
@@ -0,0 +1,39 @@
+package com.java.alunos;
+
+public class Aluno {
+	
+	private String nome;
+	private int idade;
+	private String telefone;
+	private Notas notas;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	public Notas getNotas() {
+		return notas;
+	}
+	public void setNotas(Notas notas) {
+		this.notas = notas;
+	}
+	
+	
+	
+	
+
+}
---
+++ src/com/java/alunos/Notas.java
@@ -0,0 +1,54 @@
+package com.java.alunos;
+
+public class Notas {
+	
+	private double notaUnidade1;
+	private double notaUnidade2;
+	private double notaRecuperacao1;
+	private double notaRecuperacao2;
+	private double segudachamada;
+	private double avaliacaoFinal;
+	
+	
+	public double getNotaUnidade1() {
+		return notaUnidade1;
+	}
+	public void setNotaUnidade1(double notaUnidade1) {
+		this.notaUnidade1 = notaUnidade1;
+	}
+	public double getNotaUnidade2() {
+		return notaUnidade2;
+	}
+	public void setNotaUnidade2(double notaUnidade2) {
+		this.notaUnidade2 = notaUnidade2;
+	}
+	public double getNotaRecuperacao1() {
+		return notaRecuperacao1;
+	}
+	public void setNotaRecuperacao1(double notaRecuperacao1) {
+		this.notaRecuperacao1 = notaRecuperacao1;
+	}
+	public double getNotaRecuperacao2() {
+		return notaRecuperacao2;
+	}
+	public void setNotaRecuperacao2(double notaRecuperacao2) {
+		this.notaRecuperacao2 = notaRecuperacao2;
+	}
+	public double getSegudachamada() {
+		return segudachamada;
+	}
+	public void setSegudachamada(double segudachamada) {
+		this.segudachamada = segudachamada;
+	}
+	public double getAvaliacaoFinal() {
+		return avaliacaoFinal;
+	}
+	public void setAvaliacaoFinal(double avaliacaoFinal) {
+		this.avaliacaoFinal = avaliacaoFinal;
+	}
+	
+	
+	
+	
+
+}
