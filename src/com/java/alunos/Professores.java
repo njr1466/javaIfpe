efb9774f6fe196bbe085ffe81043a9813dcf9469
njr1466 <njr1466@gmail.com>
2015-09-19 15:13:46 -0300

    adicionando a classe professores

---
+++ src/com/java/alunos/Professores.java
@@ -0,0 +1,24 @@
+package com.java.alunos;
+
+public class Professores {
+	
+	private String nome;
+	private String disciplina;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getDisciplina() {
+		return disciplina;
+	}
+	public void setDisciplina(String disciplina) {
+		this.disciplina = disciplina;
+	}
+	
+	
+	
+
+}
